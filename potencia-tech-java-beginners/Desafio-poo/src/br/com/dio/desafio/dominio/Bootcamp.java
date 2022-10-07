1af50d0cf4ea75591994ad52b1c397f0bf52f763
NicolasMatheus <nicolasgredes@gmail.com>
2022-10-07 16:42:17 -0300

    add Bootcamp, Dev

---
+++ potencia-tech-java-beginners/Desafio-poo/src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,61 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    private final LocalDate DATA_INICIAL = LocalDate.now();
+    private final LocalDate DATA_FINAL = DATA_INICIAL.plusDays(45);
+    private String nome;
+    private String descricao;
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(DATA_INICIAL, bootcamp.DATA_INICIAL) && Objects.equals(DATA_FINAL, bootcamp.DATA_FINAL) && Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(devsInscritos, bootcamp.devsInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(DATA_INICIAL, DATA_FINAL, nome, descricao, devsInscritos, conteudos);
+    }
+}
